45300b2a568c4504e9da588237f872963e0a9c64
anugrahputriamaliahwork-lab <anugrahputriamaliahwork@gmail.com>
2026-05-20 10:45:27 +0800

    kimia_farma_analysis.sql

--- kimia_farma_analysis.sql
+++ kimia_farma_analysis.sql
@@ -1,16 +1,16 @@
 --Syntax Querying data dari 4 tabel menjadi satu tabel
 SELECT
   ft.transaction_id,
-  ft.date AS transaction_date, -- merubah nama kolom dari "date" ke "transaction_date"
+  ft.date AS transaction_date, -- mengubah nama kolom dari "date" ke "transaction_date"
   kc.branch_id,
   kc.branch_name,
   kc.kota,
   kc.provinsi,
-  kc.rating AS rating_cabang, -- merubah nama kolom dari "rating" ke "rating_cabang"
+  kc.rating AS rating_cabang, -- mengubah nama kolom dari "rating" ke "rating_cabang"
   ft.customer_name,
   pd.product_id,
   pd.product_name,
-  ft.price AS actual_price, -- merubah nama kolom dari "price" ke "actual_price"
+  ft.price AS actual_price, -- mengubah nama kolom dari "price" ke "actual_price"
   ft.discount_percentage,
   CASE 
       WHEN ft.price > 500000 THEN 0.30
@@ -18,7 +18,7 @@ SELECT
       WHEN ft.price > 100000 THEN 0.20
       WHEN ft.price > 50000  THEN 0.15
       ELSE 0.10
-    END AS presentase_gros_laba, -- CASE WHEN adalah operasi conditional logic untuk mengisi baris kolom berdasarkan kondisi yang diberikan
+    END AS presentase_gros_laba, 
   ft.price - (ft.price * ft.discount_percentage) AS nett_sales, -- menghitung nett sales = harga -(harga*diskon)
   ROUND(
   (ft.price - (ft.price * ft.discount_percentage)) *
@@ -30,7 +30,7 @@ SELECT
     ELSE 0.10
   END, 
 2) AS nett_profit, -- menghitung nett profit = nett sales * presentase gros laba
-  ft.rating AS rating_transaksi -- merubah nama kolom dari "rating" ke "rating_transaksi"
+  ft.rating AS rating_transaksi -- mengubah nama kolom dari "rating" ke "rating_transaksi"
 FROM kimia_farma.kf_final_transaction ft
 --ini adalah proses join dari tabel kf_final_transaction ke kf_product dan kf_kantor_cabang
 JOIN kimia_farma.kf_product pd 
